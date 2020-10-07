b1baefb7908d03624aff2be695fcbf93aebd1ae1
Myrian Katto <myriankatto@gmail.com>
2020-10-07 17:23:13 -0300

    Banners updates

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -15,6 +15,7 @@ import banner8 from '../../assets/images/banner8.jpg';
 import banner9 from '../../assets/images/banner9.jpg';
 import banner10 from '../../assets/images/banner10.jpg';
 import banner11 from '../../assets/images/banner11.jpg';
+import banner12 from '../../assets/images/banner12.jpg';
 
 import gif1 from '../../assets/images/FORMAS1.gif';
 import gif2 from '../../assets/images/FORMAS2.gif';
@@ -71,6 +72,9 @@ function Section() {
             </div>
             <div>
               <img src={banner11} alt="Banner ONErpm Academy" />
+            </div>
+            <div>
+              <img src={banner12} alt="Banner ONErpm Academy" />
             </div>
           </Slider>
         </div>
