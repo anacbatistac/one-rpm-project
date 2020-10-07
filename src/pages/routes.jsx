3e53fbec645ba4f6c9b230f66bcd4f2b5a919588
Ana Batista <anacbatistac@gmail.com>
2020-10-06 23:26:28 -0300

    Https routes update 1.2

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Home from './Home';
-import Brasil from './Brasil';
-import Latam from './Latam';
+import Home from './Home'
+import Brasil from './Brasil'
+import Latam from './Latam'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Home} />
-      <Route path="/brasil" exact component={Brasil} />
-      <Route path="/latam" exact component={Latam} />
+      <Route path="/brasil"  component={Brasil} />
+      <Route path="/latam"  component={Latam} />
     </BrowserRouter>
   );
 }
